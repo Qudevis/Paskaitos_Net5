462ba8b9d72273d2cf9750445a6f02f14a9451c3
Justas Kvederis <justas@computech.ie>
2024-06-06 19:33:42 +0300

    another commit

--- Paskaita_13/Program.cs
+++ Paskaita_13/Program.cs
@@ -1,12 +1,25 @@
-﻿namespace Paskaita_13
+﻿using System.Text;
+
+namespace Paskaita_13
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Printinu");
-            Console.WriteLine();
+            string[] sentences = { "Hello how are you", "This is a nice day", "Are you okay", "I am doing great" };
+
+            int[] numbers = { 4, 7, 2, 6, 9, 1, 5, 7, 8, 3, 5, 4 };
+
+
+            foreach (int number in numbers) // number = numbers[0] .... number = numbers[1]... number = numbers[2]
+            {
+                Console.WriteLine(Squared(number));
+            }
+        }
+
+        public static int Squared(int number)
+        {
+            return number * number;
         }
     }
 }
